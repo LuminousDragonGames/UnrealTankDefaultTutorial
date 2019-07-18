bad0e5c1ead0bd955f2e483c510f5c2b5010395d
SundererKing <sundererking@gmail.com>
2019-07-17 20:26:08 -0700

    The tank movement is fucntional.

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,9 +1,24 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
+
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+    if (!LeftTrackToSet || !RightTrackToSet) { return; }
+    LeftTrack = LeftTrackToSet;
+    RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
+    LeftTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(Throw);
+}
 
-    UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw );
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+    LeftTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(-Throw);
 }
